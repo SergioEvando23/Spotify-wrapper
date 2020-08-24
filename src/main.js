4d8de9adcc2e3fbefb8ed6f0ffaee967c7239aef
SergioEvando23 <sergioevandocosta@gmail.com>
2020-08-24 18:12:53 -0300

    calledOnce

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-export const search = () => {}
+export const search = () =>
+  fetch('https://spotify.com')
 
-export const searchArtists = () => {}
+export const searchArtists = () => { }
 
-export const searchAlbuns = () => {}
+export const searchAlbuns = () => { }
 
-export const searchTracks = () => {}
+export const searchTracks = () => { }
 
-export const searchPlayLists = () => {}
+export const searchPlayLists = () => { }
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,31 +1,47 @@
-import  { expect } from 'Chai';
+import Chai, { expect } from 'Chai';
 import { search, searchAlbuns, searchArtists, searchTracks, searchPlayLists } from '../src/main.js';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+Chai.use(sinonChai);
+sinonStubPromise(sinon);
 
-describe('spotify wrepper' , () => {
-    discribe('smoke tests', () =>{
-        //search generico que pode buscar por mais de um tipo
-        //search albuns
-        //search artistes
-        //search tracks
-        //search play lists
+global.fetch = require('node-fetch')
 
-        it ('should exist the search method', () =>{
-            expect(search).to.exist;
-        });
+describe('spotify wrepper', () => {
+  describe('smoke tests', () => {
+    //search generico que pode buscar por mais de um tipo
+    //search albuns
+    //search artistes
+    //search tracks
+    //search play lists
 
-        it ('should exist the searchAlbuns method', () =>{
-            expect(searchAlbuns).to.exist;
-        });
-
-        it ('should exist the searchArtists method', () =>{
-            expect(searchArtists).to.exist;
-        });
-
-        it ('should exist the searchTracks method', () =>{
-            expect(searchTracks).to.exist;
-        });
-        it ('should exist the searchPlayLists method', () =>{
-            expect(searchPlayLists).to.exist;
-        });
+    it('should exist the search method', () => {
+      expect(search).to.exist;
     });
+
+    it('should exist the searchAlbuns method', () => {
+      expect(searchAlbuns).to.exist;
+    });
+
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+    it('should exist the searchPlayLists method', () => {
+      expect(searchPlayLists).to.exist;
+    });
+  });
+
+  describe('Generic Search', () => {
+    it('should call fetch function', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
+      const artists = search();
+
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+  });
 });
