12725ecf11447545fd75767a444b23c7c38e6903
SergioEvando23 <sergioevandocosta@gmail.com>
2020-08-24 17:38:49 -0300

    smoke tests

---
+++ src/main.js
@@ -0,0 +1,9 @@
+export const search = () => {}
+
+export const searchArtists = () => {}
+
+export const searchAlbuns = () => {}
+
+export const searchTracks = () => {}
+
+export const searchPlayLists = () => {}
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,84 +1,31 @@
-/*                      CALCULADORA TESTES
-***************************************************************************
-import { expect } from 'chai';
-import { assert } from 'chai';
-import { sum, sub, div, mult } from '../src/main.js';
-const { should } = require('chai');
+import  { expect } from 'Chai';
+import { search, searchAlbuns, searchArtists, searchTracks, searchPlayLists } from '../src/main.js';
 
- describe('calc', () => {
-    //smoke testes(testes mais simples)
-    describe('smoke testes', () => {
+describe('spotify wrepper' , () => {
+    discribe('smoke tests', () =>{
+        //search generico que pode buscar por mais de um tipo
+        //search albuns
+        //search artistes
+        //search tracks
+        //search play lists
 
-        it('should exist the method `sum`', () => {
-            expect(sum).to.exist;
-            assert.typeOf(sum, 'function');
-        });
-
-        it('should exist the method `sub`', (S) => {
-            expect(sub).to.exist;
-            assert.typeOf(sub, 'function');
+        it ('should exist the search method', () =>{
+            expect(search).to.exist;
         });
 
-        it('should exist the method `div`', () => {
-            expect(div).to.exist;
-            assert.typeOf(div, 'function');
+        it ('should exist the searchAlbuns method', () =>{
+            expect(searchAlbuns).to.exist;
         });
 
-        it('should exist the method `mult`', () => {
-            expect(mult).to.exist;
-            assert.typeOf(mult, 'function');
-        });
-    });
-
-    describe('sum', () => {
-        it('should return 4 when `sum(2,2)`', () => {
-            expect(sum(2, 2)).to.be.equal(4);
-        });
-    });
-
-    describe('sub', () => {
-        it('should return 4 when `sub(6,2)`', () => {
-            expect(sub(6, 2)).to.be.equal(4);
+        it ('should exist the searchArtists method', () =>{
+            expect(searchArtists).to.exist;
         });
 
-        it('should return -4 when `sub(6,10)`', () => {
-            expect(sub(6, 10)).to.be.equal(-4);
+        it ('should exist the searchTracks method', () =>{
+            expect(searchTracks).to.exist;
         });
-    });
-    describe('div', () => {
-        it('should return 4 when `div(8,2)`', () => {
-            expect(div(8, 2)).to.be.equal(4);
+        it ('should exist the searchPlayLists method', () =>{
+            expect(searchPlayLists).to.exist;
         });
-        it('should return `Não é possivel divisão por zero!` when divi by 0', () => {
-            expect(div(4, 0)).to.be.equal('Não é possivel divisão por zero!');
-        });
-    });
-    describe('mult', () => {
-        it('should return 4 when `mult(2,2)`', () => {
-            expect(mult(2, 2)).to.be.equal(4);
-        });
-    });
-});
-*************************************************************************************************
-*/
-import { expect } from 'chai';
-import fizzBuss from '../src/main'
-describe('fizzBuss', () => {
-    it('should return `fizz` when multiple of 3', () => {
-        expect(fizzBuss(3)).to.be.equal('fizz');
-        expect(fizzBuss(6)).to.be.equal('fizz');
-    });
-    it('should return `buss` when multiple of 5', () => {
-        expect(fizzBuss(5)).to.be.equal('Buss');
-        expect(fizzBuss(10)).to.be.equal('Buss');
-    });
-    it('should return `fizzBuss` when multiple of 3 and 5', () => {
-        expect(fizzBuss(15)).to.be.equal('fizzBuss');
-    });
-    it('should return the number when non-multiple', () => {
-        expect(fizzBuss(7)).to.be.equal(7);
-    });
-    it('should return`0` when 0', () => {
-        expect(fizzBuss(0)).to.be.equal(0);
     });
 });
